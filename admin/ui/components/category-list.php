c38177fd6f2cfef1ac8b7450bbacafd3310edf78
Fida Waseque Choudhury <fida.waseque@gmail.com>
2024-05-30 10:47:59 +0600

    reafctor(ui): add category list component

---
+++ admin/ui/components/category-list.php
@@ -0,0 +1,27 @@
+<article class="blogify-category-list">
+    <section class="blogify-header">
+        <span class="blogify-title">My Categories</span>
+        <button type="button" class="blogify-primary">Create</button>
+    </section>
+    <section class="blogify-items">
+        <span class="blogify-category-info">
+            <span class="blogify-category-name">Herbs</span>
+            <span class="blogify-category-stats">Draft: 8 · Scheduled: 2 · Published : 16</span>
+        </span>
+        <span class="blogify-category-info">
+            <span class="blogify-category-name">Herbs</span>
+            <span class="blogify-category-stats">Draft: 8 · Scheduled: 2 · Published : 16</span>
+        </span>
+        <span class="blogify-category-info">
+            <span class="blogify-category-name">Herbs</span>
+            <span class="blogify-category-stats">Draft: 8 · Scheduled: 2 · Published : 16</span>
+        </span>
+        <span class="blogify-category-info">
+            <span class="blogify-category-name">Herbs</span>
+            <span class="blogify-category-stats">Draft: 8 · Scheduled: 2 · Published : 16</span>
+        </span>
+        <span class="blogify-category-info">
+            <span class="blogify-category-name">Herbs</span>
+            <span class="blogify-category-stats">Draft: 8 · Scheduled: 2 · Published : 16</span>
+        </span>
+</article>
